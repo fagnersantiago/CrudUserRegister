8eb32b71ced99d880faad4c833059d7cdd80db99
Fagner Santiago <fagner.santiago@hotmail.com>
2022-03-14 08:46:40 -0300

    ajustes nos arquivos

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,18 +1,3 @@
-// import { createConnection, getConnectionOptions } from "typeorm";
-
-// interface IOptions {
-//   host: string;
-// }
-
-// getConnectionOptions().then((options) => {
-//   const newOptions = options as IOptions;
-//   newOptions.host = "192.168.99.100";
-
-//   createConnection({
-//     ...options,
-//   });
-// });
-
 import { Connection, createConnection, getConnectionOptions } from "typeorm";
 
 export default async (): Promise<Connection> => {
--- src/repositories/UserRepository.ts
+++ src/repositories/UserRepository.ts
@@ -6,23 +6,6 @@ class UserRepository extends Repository<User> {
   async findByNickname(nickname: string): Promise<User> {
     return this.findOne({ nickname });
   }
-  // async store({
-  //   name,
-  //   last_name,
-  //   nickname,
-  //   adress,
-  //   bio,
-  // }: ICreateUser): Promise<User> {
-  //   const userRepository = getCustomRepository(UserRepository);
-  //   const user = await userRepository.create({
-  //     name,
-  //     last_name,
-  //     nickname,
-  //     adress,
-  //     bio,
-  //   });
-  //   return userRepository.save(user);
-  // }
 }
 
 export default UserRepository;
